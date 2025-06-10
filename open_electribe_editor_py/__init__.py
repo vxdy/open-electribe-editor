fb4dcb56bb7c2b202b21feb8a23d3359681ba274
Marco B <60035996+vxdy@users.noreply.github.com>
2025-06-10 12:18:41 +0200

    Add sample ESX loading test

---
+++ open_electribe_editor_py/__init__.py
@@ -0,0 +1,20 @@
+"""Python reimplementation stubs for Open Electribe Editor."""
+
+from . import esx_constants
+from .extended_byte_buffer import ExtendedByteBuffer
+from . import esx_util
+from .esx_file import ESXFile
+from .esx_structs import ESXData, GlobalParameters, Patterns, Songs, Samples, Sample
+
+__all__ = [
+    "esx_constants",
+    "ExtendedByteBuffer",
+    "esx_util",
+    "ESXFile",
+    "ESXData",
+    "GlobalParameters",
+    "Patterns",
+    "Songs",
+    "Samples",
+    "Sample",
+]
